3e104e61d927d5fb875f3bab238329172a9cc51a
rinush <rinu098@gmail.com>
2021-03-21 19:46:15 -0400

    Add files

--- src/main/java/com/example/wbdvsp2103shailiserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsp2103shailiserverjava/controllers/WidgetController.java
@@ -25,58 +25,41 @@ public class WidgetController {
   WidgetService service;
 
   @PostMapping( "/api/topics/{tid}/widgets" )
-  public Widget createWidget(
+  public Widget createWidgetForTopic(
           @PathVariable("tid") String topicId,
-          @RequestBody Widget w
-  ) {
-    return service.createWidget( topicId, w );
+          @RequestBody Widget widget) {
+    widget.setTopicId(topicId);
+    return service.createWidgetForTopic(widget);
   }
 
   @GetMapping( "/api/topics/{tid}/widgets" )
   public List<Widget> findWidgetsForTopic (
           @PathVariable("tid") String topicId
   ) {
-    return service.findWidgetsForTopic( topicId );
+    return service.findWidgetsForTopic(topicId);
   }
 
-  // Actually not used, as of now
   @GetMapping( "/api/widgets" )
   public List<Widget> findAllWidgets() {
     return service.findAllWidgets();
   }
 
-  // Also not used, as of now
   @GetMapping( "/api/widgets/{wid}" )
   public Widget findWidgetById(
-          @PathVariable("wid") Long widgetId
-  ) {
-    return service.findWidgetById( widgetId );
+          @PathVariable("wid") Long id) {
+    return service.findWidgetById(id);
   }
 
-
-
-  // PUTS
-  // ======
+  @DeleteMapping( "/api/widgets/{wid}" )
+  public Integer deleteWidget(@PathVariable("wid") Long id) {
+    return service.deleteWidget(id);
+  }
 
   @PutMapping( "/api/widgets/{wid}" )
   public Integer updateWidget(
-          @PathVariable("wid") Long widgetId,
-          @RequestBody Widget w
-  ) {
-    return service.updateWidget( widgetId, w);
+          @PathVariable("wid") Long id,
+          @RequestBody Widget widget) {
+    return service.updateWidget(id, widget);
   }
-
-
-
-  // DELETES
-  // =========
-
-  @DeleteMapping( "/api/widgets/{wid}" )
-  public Integer deleteWidget(
-          @PathVariable("wid") Long widgetId
-  ) {
-    return service.deleteWidget( widgetId );
-  }
-
 }
 
--- src/main/java/com/example/wbdvsp2103shailiserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2103shailiserverjava/services/WidgetService.java
@@ -13,75 +13,74 @@ import java.util.List;
 public class WidgetService {
   private List<Widget> widgets = new ArrayList<Widget>();
   {
-    // Dummy value all associated with a non-existent topic ID in the real data. Ima leave this
-    //   kinda just because.
-    Widget w1 = new Widget( 123l, "ABC123", "HEADING", 1, "Widgets for topic ABC");
-    Widget w2 = new Widget( 234l, "ABC123", "PARAGRAPH", 1, "Lorem Ipsum");
-    Widget w3 = new Widget( 345l, "ABC234", "HEADING", 2, "Welcome to Widget List!");
-    Widget w4 = new Widget( 456l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
-    Widget w5 = new Widget( 567l, "ABC234", "PARAGRAPH", 1, "Idkkkk");
-
+    Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Welcome to Widgets");
+    Widget w2 = new Widget(234l, "ABC234", "PARAGRAPH", 1, "This is a paragraph");
+    Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Welcome to WebDev");
+    Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Lorem ipsum");
     widgets.add(w1);
     widgets.add(w2);
     widgets.add(w3);
     widgets.add(w4);
-    widgets.add(w5);
   }
 
 
 
-  public Widget createWidget( String topicId, Widget w ) {
-    w.setTopicId( topicId );
-    w.setId(new Date().getTime());
-    widgets.add( w );
-    return w;
-  }
-
-
-  public List<Widget> findWidgetsForTopic( String topicId ) {
-    List<Widget> ws = new ArrayList<Widget>();
-    for ( Widget w : widgets ) {
-      if (w.getTopicId().equals( topicId )) {
-        ws.add( w );
-      }
-    }
-    return ws;
-  }
-
-
-  public Integer updateWidget( Long widgetId, Widget w ) {
-    for( int i = 0; i < widgets.size(); i++ ) {
-      if(widgets.get(i).getId().equals(widgetId)) {
-        widgets.set(i, w);
-        return 1;
-      }
-    }
-    return -1;
-  }
-
-
-
-  public Integer deleteWidget( Long widgetId ) {
-    for( int i = 0; i < widgets.size(); i++ ) {
-      if(widgets.get(i).getId().equals(widgetId)) {
-        widgets.remove(i);
-        return 1;
-      }
-    }
-    return -1;
+  public Widget createWidgetForTopic(Widget widget) {
+    Long id = new Date().getTime();
+    widget.setId(id);
+    widgets.add(widget);
+    return widget;
   }
 
   public List<Widget> findAllWidgets() {
     return widgets;
   }
 
-  public Widget findWidgetById( Long widgetId ) {
-    for( int i = 0; i < widgets.size(); i++ ) {
-      if(widgets.get(i).getId().equals(widgetId)) {
-        return widgets.get(i);
+  public List<Widget> findWidgetsForTopic(String topicId) {
+    List<Widget> ws = new ArrayList<Widget>();
+    for(Widget w : widgets) {
+      if(w.getTopicId().equals(topicId)) {
+        ws.add(w);
+      }
+    }
+    return ws;
+  }
+
+  public Widget findWidgetById(Long id) {
+    for(Widget w: widgets) {
+      if(w.getId().equals(id)) {
+        return w;
       }
     }
     return null;
+  }
+
+  public Integer updateWidget(Long id, Widget newWidget) {
+    for(int i=0; i<widgets.size(); i++) {
+      Widget w = widgets.get(i);
+      if(w.getId().equals(id)) {
+        widgets.set(i, newWidget);
+        return 1;
+      }
+    }
+    return -1;
+  }
+
+
+
+  public Integer deleteWidget(Long id) {
+    int index = -1;
+    for(int i=0; i<widgets.size(); i++) {
+      Widget w = widgets.get(i);
+      if(w.getId().equals(id)) {
+        index = i;
+      }
+    }
+    if(index >= 0) {
+      widgets.remove(index);
+      return 1;
+    }
+    return -1;
   }
 
 
