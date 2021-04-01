f2cfd3316e71e12b8cc6f6cdc8a6bcf031dee542
rinush <rinu098@gmail.com>
2021-03-31 22:34:30 -0400

    Add updates

--- src/main/java/com/example/wbdvsp2103shailiserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsp2103shailiserverjava/controllers/WidgetController.java
@@ -18,46 +18,46 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "*")
 public class WidgetController {
-  
   @Autowired
   WidgetService service;
 
-  @PostMapping( "/api/topics/{tid}/widgets" )
+  @PostMapping("/api/topics/{tid}/widgets")
   public Widget createWidgetForTopic(
           @PathVariable("tid") String topicId,
           @RequestBody Widget widget) {
     widget.setTopicId(topicId);
-    return service.createWidgetForTopic(widget);
+    return service.createWidget(widget);
   }
 
-  @GetMapping( "/api/topics/{tid}/widgets" )
-  public List<Widget> findWidgetsForTopic (
+  @GetMapping("/api/topics/{tid}/widgets")
+  public List<Widget> findWidgetsForTopic(
           @PathVariable("tid") String topicId
   ) {
     return service.findWidgetsForTopic(topicId);
   }
 
-  @GetMapping( "/api/widgets" )
-  public List<Widget> findAllWidgets() {
-    return service.findAllWidgets();
-  }
-
-  @GetMapping( "/api/widgets/{wid}" )
-  public Widget findWidgetById(
-          @PathVariable("wid") Long id) {
-    return service.findWidgetById(id);
-  }
-
-  @DeleteMapping( "/api/widgets/{wid}" )
-  public Integer deleteWidget(@PathVariable("wid") Long id) {
-    return service.deleteWidget(id);
-  }
-
-  @PutMapping( "/api/widgets/{wid}" )
+  @PutMapping("/api/widgets/{wid}")
   public Integer updateWidget(
           @PathVariable("wid") Long id,
           @RequestBody Widget widget) {
     return service.updateWidget(id, widget);
   }
+
+  @DeleteMapping("/api/widgets/{wid}")
+  public Integer deleteWidget(@PathVariable("wid") Long id) {
+    return service.deleteWidget(id);
+  }
+
+  @GetMapping("/api/widgets")
+  public List<Widget> findAllWidgets() {
+    return service.findAllWidgets();
+  }
+
+  @GetMapping("/api/widgets/{wid}")
+  public Widget findWidgetById(
+          @PathVariable("wid") Long id) {
+    return service.findWidgetById(id);
+  }
+
 }
 
